c321cdb0849cc8e6553cc0958dd1145edeb446ac
yfsenna <46488240+yfsenna@users.noreply.github.com>
2019-01-17 09:29:40 -0200

    Update acharNumero.js

--- fase04/ex07/acharNumero.js
+++ fase04/ex07/acharNumero.js
@@ -4,10 +4,10 @@ function acharNumero(num){
     if (num < 5) {
         return "a";
         }
-    if (num > 5 && num < 10){
+    if (num >= 5 && num <= 10){
         return "b";
     }
-     if (num > 10 && num < 100){ 
+     if (num > 10 && num <= 100){ 
         return "c";
     }
     if (num > 100){
