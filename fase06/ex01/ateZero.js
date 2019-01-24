53a7369fde4860c07bf37f4b787beb752b6f0e63
yfsenna <46488240+yfsenna@users.noreply.github.com>
2019-01-24 09:42:29 -0200

    ate zero

---
+++ fase06/ex01/ateZero.js
@@ -0,0 +1,22 @@
+function ateZero(x) {
+    y=[];
+    
+    if(x>0)
+    
+    for(i=0;i<=x;i++){
+        
+        y.push(i);
+}
+        else{
+            
+        for(i=x;i<=0;i++)
+        
+        y.push(i);
+}        
+        return y;
+
+}
+
+
+
+
