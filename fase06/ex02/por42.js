7727e500d017b8678f6d02b0c0844ee24aeade6a
yfsenna <46488240+yfsenna@users.noreply.github.com>
2019-01-24 11:00:30 -0200

    Update por42.js

--- fase06/ex02/por42.js
+++ fase06/ex02/por42.js
@@ -1,10 +1,10 @@
 function por42(x, y) {
-    x=(Math.floor(x/42+1)*42+42);
+    x=(Math.ceil(x/42+1)*42);
     if (x<=y){
     return x;
     }
     else{ 
-        return ("Não encontrado");
+        return ("NÃ£o encontrado");
 
 }
 }
