62132632040bc0ca6fcb3f1a0b4229dc3ee9e5b7
yfsenna <46488240+yfsenna@users.noreply.github.com>
2019-01-29 10:18:37 -0200

    inverter

---
+++ fase06/ex07/inverter.js
@@ -0,0 +1,13 @@
+
+function inverter (item){
+    if (typeof item === 'string'){
+        var a = item.split("");
+        var b = a.reverse();
+        var c = b.join("");
+        return c;
+
+    }else{
+    item.reverse();
+        return item;
+    }
+}
