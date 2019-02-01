728cb930c94659f618c5a8c9f79a6d4a85a2fdf0
yfsenna <46488240+yfsenna@users.noreply.github.com>
2019-02-01 11:11:35 -0200

    pinguins

---
+++ fase07/ex06/pinguins.js
@@ -0,0 +1,11 @@
+function pinguins(arr){
+    
+  roupa={cabeca:"oculos de sol",camisa:"jaqueta polar",pes:"tenis"}
+  
+        for (i=0; i<arr.length; i++){
+
+        arr[i].roupa = roupa
+    }
+    return arr
+}
+
